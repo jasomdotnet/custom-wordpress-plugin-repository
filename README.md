268bc9ab81a0a4cca5ad22dcff47feb345d16fe3
Jasom Dotnet <jasom.net@gmail.com>
2020-02-18 23:47:50 +0100

    Various changes

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # custom-wordpress-plugin-repository
-Self-hosted WordPress plugin (and theme) repository so your custom plugins can be updated automatically. I have released this repository so you can start build your solution upon.
+Private self-hosted Wordpress plugin (and theme) repository so your custom plugins (and themes) can be updated automatically. I have released this code so you can start building your solution upon.
 
 ## What are minimum requirements
-It runs for me on PHP 7.0. It requires no database, access to shell command and unzip class (both are not a big deal even on shared web-hosting).
+I don't know min. requirements, but it runs for me on PHP 7.3. It needs no database and requires access to shell + unzip class (both are not a big deal even on shared web-hosts).
 
-## How to configure repository server
-1. Fill `Config` class with your data, namely domain, path to the repository folder, latest tested WordPress version for all your plugins, minimum required WordPress version for all your plugins and array of slugs for plugins hosted within your private repository server
+## How to configure the repository server?
+1. Fill `Config` class with your data, namely: domain, the path to the repository folder, latest tested WordPress version for all hosted plugins, minimum required WordPress version for all hosted plugins and an array of slugs belonging to plugins hosted within your private repository server
 2. Set web accessible cronjob `https://www.example.com/repositoryfolder/get-info.php?action=cron` to every 6 hours
 
-## How do looks like plugin header
-Repo script supports plugin with structure : `mypluginslug1/mypluginslug1.php` while plugin header looks as follows:
+## How do looks like a plugin header?
+Repo script supports out of the box plugins with structure : `mypluginslug1/mypluginslug1.php` while plugin header looks as follows:
 
 ```
 /**
@@ -25,33 +25,31 @@ Repo script supports plugin with structure : `mypluginslug1/mypluginslug1.php` w
  * Domain Path: /languages
  */
  ```
- 
- Version tags `1.1`, `1.2`, `1.3`...
+Tag versions like `1.1`, `1.2`, `1.3`...
 
-
-## How to add new plugin to repository server
+## How to add a new plugin to repository server?
 1. Create plugin with following structure: `mypluginslug1/mypluginslug1.php`
-2. Zip plugin and upload to the repository folder
-3. Add new plugin slug to the array represented by constant PLUGINS in Config class
-4. Add 2 banners to the repository folder with name (and dimensions) like `mypluginslug1-banner-1544x500.jpg` and `mypluginslug1-banner-772x250.jpg`
+2. Zip plugin and upload it to repository folder
+3. In Config class, add a new plugin slug to the array represented by constant PLUGINS
+4. Add 2 banners to the repository folder with the name (and dimensions) like `mypluginslug1-banner-1544x500.jpg` and `mypluginslug1-banner-772x250.jpg`
 5. Run cronjob above
-6. See final jSon with under `https://www.example.com/repositoryfolder/get-info.php?slug=mypluginslug1`
+6. See final jSon under `https://www.example.com/repositoryfolder/get-info.php?slug=mypluginslug1`
 
-## How to add new plugin release
+## How to add a new plugin release?
 1. Remove old `mypluginslug1.zip`
 2. Upload new `mypluginslug1.zip`
 3. Cronjob automatically does the rest
 
 ## What code needs to be added to plugin for synchronization
 
-I have attached simple plugin in mypluginslug1.zip so read the code. It is also explained on repository homepage.
+I have attached a simple plugin in mypluginslug1.zip so read the code. It is also explained on repository homepage.
 
 ## Where I can find more info
-Script home page is here https://www.jasom.net/
-This code was inspired by @rudrastyh post [Self-Hosted Plugin Update](https://rudrastyh.com/wordpress/self-hosted-plugin-update.html) who took inspiration (I quess) from @YahnisElsts [wp-update-server](https://github.com/YahnisElsts/wp-update-server)
+Script home page is [here](https://www.jasom.net/). This code was inspired by [@rudrastyh](https://github.com/rudrastyh)'s post [Self-Hosted Plugin Update](https://rudrastyh.com/wordpress/self-hosted-plugin-update.html) who took inspiration (I guess) from [@YahnisElsts](https://github.com/YahnisElsts)'s [wp-update-server](https://github.com/YahnisElsts/wp-update-server)
 
 ## Credits
 
 Yahnis Elsts https://w-shadow.com
 Misha Rudrastyh https://rudrastyh.com
 Jasom Dotnet https://www.jasom.net
+.
---
+++ mypluginslug1/mypluginslug1.php
@@ -0,0 +1,161 @@
+<?php
+
+/**
+ * Plugin Name: Myplugin One
+ * Description: Demonstration plugin.
+ * Version: 1.0
+ * Author: Jasom Dotnet
+ * Author URI: https://www.jasom.net
+ * Plugin URI: https://www.example.com
+ * License: MIT License
+ * License URI: http://opensource.org/licenses/MIT
+ * Text Domain: mypluginslug1
+ * Domain Path: /languages
+ */
+###############
+# Defaults and options
+###############
+define( 'PREFIX_PLUGIN_NAME', 'Myplugin One' );
+define( 'PREFIX_PLUGIN_VERSION', '1.0' );
+define( 'PREFIX_DOMAIN', 'https://www.example.com' );
+define( 'PREFIX_REPOFOLDER', 'repo' );
+
+###############
+# Logic
+###############
+
+function prefix_add_code_to_the_footer() {
+
+    echo PHP_EOL . '<!-- it_works -->' . PHP_EOL;
+
+}
+
+add_action( 'wp_footer', 'prefix_add_code_to_the_footer' );
+
+###############
+# Automatic updates
+###############
+
+/*
+ * Plugin pop-up when new realase is out
+ */
+
+function prefix_plugin_info( $res, $action, $args ) {
+
+    // Do nothing if this is not about getting plugin information
+    if ($action !== 'plugin_information') {
+        return false;
+    }
+
+    // Do nothing if it is not our plugin
+    if ('mypluginslug1' !== $args->slug) {
+        return $res;
+    }
+
+    // Trying to get from cache first, to disable cache see https://rudrastyh.com/wordpress/self-hosted-plugin-update.html
+    if (false == $remote = get_transient( 'prefix_upgrade_mypluginslug1' )) {
+
+        $remote = wp_remote_get( PREFIX_DOMAIN . '/' . PREFIX_REPOFOLDER . '/get-info.php?slug=mypluginslug1&action=info', array(
+            'timeout' => 10,
+            'headers' => array(
+                'Accept' => 'application/json'
+            ))
+        );
+
+        if (!is_wp_error( $remote ) && isset( $remote[ 'response' ][ 'code' ] ) && $remote[ 'response' ][ 'code' ] == 200 && !empty( $remote[ 'body' ] )) {
+            set_transient( 'prefix_upgrade_mypluginslug1', $remote, 21600 ); // 6 hours cache
+        }
+    }
+
+    if (!is_wp_error( $remote )) {
+
+        $remote = json_decode( $remote[ 'body' ] );
+
+        $res = new stdClass();
+        $res->name = $remote->name;
+        $res->slug = $remote->slug;
+        $res->version = $remote->version;
+        $res->tested = $remote->tested;
+        $res->requires = $remote->requires;
+        $res->author = $remote->author;
+        $res->author_profile = $remote->author_homepage;
+        $res->download_link = $remote->download_link;
+        $res->trunk = $remote->download_link;
+        $res->last_updated = $remote->last_updated;
+        $res->sections = array(
+            'description' => $remote->sections->description, // description tab
+            'installation' => $remote->sections->installation, // installation tab
+        );
+        $res->banners = array(
+            'low' => $remote->banners->low,
+            'high' => $remote->banners->high,
+        );
+
+        return $res;
+    }
+
+    return false;
+
+}
+
+add_filter( 'plugins_api', 'prefix_plugin_info', 20, 3 );
+
+/**
+ * Plugin update
+ */
+function prefix_push_update( $transient ) {
+
+    if (empty( $transient->checked )) {
+        return $transient;
+    }
+
+    // trying to get from cache first, to disable cache comment 10,20,21,22,24
+    if (false == $remote = get_transient( 'prefix_upgrade_mypluginslug1' )) {
+        // info.json is the file with the actual plugin information on your server
+        $remote = wp_remote_get( PREFIX_DOMAIN . '/' . PREFIX_REPOFOLDER . '/get-info.php?slug=mypluginslug1&action=update', array(
+            'timeout' => 10,
+            'headers' => array(
+                'Accept' => 'application/json'
+            ))
+        );
+
+        if (!is_wp_error( $remote ) && isset( $remote[ 'response' ][ 'code' ] ) && $remote[ 'response' ][ 'code' ] == 200 && !empty( $remote[ 'body' ] )) {
+            set_transient( 'prefix_upgrade_mypluginslug1', $remote, 21600 ); // 6 hours cache
+        }
+    }
+
+    if ($remote) {
+
+        $remote = json_decode( $remote[ 'body' ] );
+
+        // your installed plugin version should be on the line below! You can obtain it dynamically of course
+        if ($remote && version_compare( PREFIX_PLUGIN_VERSION, $remote->version, '<' ) && version_compare( $remote->requires, get_bloginfo( 'version' ), '<' )) {
+            $res = new stdClass();
+            $res->slug = 'mypluginslug1';
+            // it could be just mypluginslug1.php if your plugin doesn't have its own directory (my does)
+            $res->plugin = 'mypluginslug1/mypluginslug1.php';
+            $res->new_version = $remote->version;
+            $res->tested = $remote->tested;
+            $res->package = $remote->download_link;
+            $transient->response[ $res->plugin ] = $res;
+            //$transient->checked[$res->plugin] = $remote->version;
+        }
+    }
+    return $transient;
+
+}
+
+add_filter( 'site_transient_update_plugins', 'prefix_push_update' );
+
+/**
+ * Cache the results to make update process fast
+ */
+function prefix_after_update( $upgrader_object, $options ) {
+    if ($options[ 'action' ] == 'update' && $options[ 'type' ] === 'plugin') {
+        // just clean the cache when new plugin version is installed
+        delete_transient( 'prefix_upgrade_mypluginslug1' );
+    }
+
+}
+
+add_action( 'upgrader_process_complete', 'prefix_after_update', 10, 2 );
